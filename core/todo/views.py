f71dae1c3c87b8f725a8bce6725c6ef4005c59dc
Thomas Deruere <t.deruere@gmail.com>
2024-09-18 10:58:59 +0200

    query to index.html

--- core/todo/views.py
+++ core/todo/views.py
@@ -2,10 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.db.models import Q
 from .models import Task
+
 # Create your views here.
+
 def index(request):
     tasks = Task.objects.filter(
         Q(status="TODO") | Q(status="DOING")
     )
-    result = [str(t) for t in tasks]
-    return HttpResponse("Hello there general Kenobi" + str(result))
+    context = {
+        "tasks": tasks
+    }
+    # result = [str(t) for t in tasks]
+    return render(request,
+                  'todo/index.html',
+                  context=context
+                  )
